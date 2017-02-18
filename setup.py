8c85a95d1acca65150a973d88581a711017cf55b
Arturo Ruiz Mañas <arruma2160@gmail.com>
2017-02-18 02:10:49 +0000

    Some more metadata for our setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,12 @@ from distutils.core import setup
 setup(
         name='pyPSP405',
         version='0.1dev',
+        author="Arturo Ruiz Mañas",
+        author_email="arruma2160@gmail.com",
         packages=['pypsp405',],
+        classifiers=[
+            'Programming Language :: Python',
+            'Intended Audience :: Science/Research',
+        ],
+        install_requires=['pyserial'],
       )
